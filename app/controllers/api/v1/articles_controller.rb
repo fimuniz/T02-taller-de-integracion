4308d1d3988f40314f19dc7832621c227281a7bd
Francisca Muñiz <fimuniz@uc.cl>
2018-04-02 18:19:23 -0300

    News funcionando

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,47 @@
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+
+      def index
+        articles = Article.order('created_at DESC');
+        render json: {status: 'OK', message: 'bla', data:articles}, status: :ok
+      end
+
+      def show
+        article = Article.find(params[:id])
+        render json: {status: 'OK', message: 'bla', data:article}, status: :ok
+      end
+
+      def create
+        article = Article.new(article_params)
+        if article.save
+        render json: {status: 'OK', message: 'bla', data:article}, status: :ok
+        else
+        render json: {status: 'NOT OK', message: 'bla', data:article.errors}, status: :ok
+        end
+      end
+
+      def destroy
+        article = Article.find(params[:id])
+        article.destroy
+        render json: {status: 'OK', message: 'bla', data:article}, status: :ok
+      end
+
+      def update
+        article = Article.find(params[:id])
+        if article.update(article_params)
+        render json: {status: 'OK', message: 'bla', data:article}, status: :ok
+        else
+        render json: {status: 'NOT OK', message: 'bla', data:article.errors}, status: :ok
+        end
+      end
+
+      private
+      def article_params
+        params.permit(:title, :subtitle, :body)
+      end
+
+    end
+
+  end
+end
